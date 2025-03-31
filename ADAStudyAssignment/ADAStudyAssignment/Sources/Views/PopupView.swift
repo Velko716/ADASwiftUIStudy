00fb60d13770beeb2b1327dd75a0a16fe4555e20
Velko <wlsgurrla716@naver.com>
2025-03-31 13:47:04 +0900

    #2 광고 팝업창 구현

---
+++ ADAStudyAssignment/ADAStudyAssignment/Sources/Views/PopupView.swift
@@ -0,0 +1,64 @@
+//
+//  PopupView.swift
+//  ADAStudyAssignment
+//
+//  Created by 김진혁 on 3/31/25.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    @Environment(\.dismiss) private var dismiss ///
+    
+    var body: some View {
+        
+        VStack {
+            Image("popupImage")
+                .resizable()
+                .scaledToFit()
+                
+            
+            Spacer()
+            
+            
+            VStack(spacing: 19) {
+                Button { print("자세히 보기") }
+                label: {
+                    Text("자세히보기")
+                        .font(.makeMedium18)
+                        .tint(.white)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 58)
+                .background(Color.green01)
+                .cornerRadius(20)
+                
+                Button { dismiss() }
+                 label: {
+                    Spacer()
+                    
+                    HStack(spacing: 0) {
+                        Text("X 닫기")
+                    } //:HSTACK
+                    .font(.mainTextLight14)
+                    .tint(.gray05)
+                    .padding(.horizontal, 19)
+                }
+                
+                
+            } //:VSTACK
+            .padding(.horizontal, 18)
+            
+            
+            
+            Spacer()
+            
+        } //: VSTACK
+        
+        
+    }
+}
+
+#Preview {
+    PopupView()
+}
