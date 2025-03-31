db370924c7693d48689fc6eb001ff7196bc19a79
Velko <wlsgurrla716@naver.com>
2025-03-31 20:19:27 +0900

    #2 탭 바 섹션 구현

---
+++ ADAStudyAssignment/ADAStudyAssignment/Sources/Views/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeView.swift
+//  ADAStudyAssignment
+//
+//  Created by 김진혁 on 3/31/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TabView {
+            Tab("Home", image: "home") {
+                EmptyView()
+            }
+            
+            Tab("Pay", image: "pay") {
+                EmptyView()
+            }
+            
+            Tab("Order", image: "order") {
+                EmptyView()
+            }
+            
+            Tab("Shop", image: "shop") {
+                EmptyView()
+            }
+            
+            Tab("Other", image: "other") {
+                EmptyView()
+            }
+        }
+        .tint(.green02) /// Assetes 이미지 오른쪽 Image Set에서 Render As를 Template Image로 변경해야함.
+        
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
